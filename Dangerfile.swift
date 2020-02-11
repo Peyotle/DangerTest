70aa46dfe3a9a8fb070e053d577241a47d2a0d43
Oleg Chernyshenko <oleg@tugboat.nz>
2020-02-11 23:17:15 +1300

    add lint

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,4 +1,12 @@
 import Danger 
 let danger = Danger()
+SwiftLint.lint()
+
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
+
+if danger.github.pullRequest.body == nil || danger.github.pullRequest.body!.isEmpty {
+    warn("Please provide a summary in the Pull Request description")
+}
+
+let additions = danger.github.pullRequest.additions
