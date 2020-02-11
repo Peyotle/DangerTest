d96f6ab98101ae5b909c2a0b686e980ead39eb4f
Oleg Chernyshenko <oleg@tugboat.nz>
2020-02-11 22:07:33 +1300

    Change VC

--- DangerTest/ViewController.swift
+++ DangerTest/ViewController.swift
@@ -5,11 +5,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
