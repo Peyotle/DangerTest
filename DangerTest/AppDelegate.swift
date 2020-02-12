347e1076b19a8124e75451a8a0e1b31a1fff5bb5
Oleg Chernyshenko <oleg.chernyshenko@verizonconnect.com>
2020-02-12 16:44:51 +1300

    some changes

--- DangerTest/AppDelegate.swift
+++ DangerTest/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
