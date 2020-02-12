0e2000954cf688974051c15d6a191b97429d7a8a
Oleg Chernyshenko <oleg.chernyshenko@verizonconnect.com>
2020-02-12 15:56:27 +1300

    Update dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,4 +1,13 @@
-import Danger 
+import Danger
 let danger = Danger()
+
+
+message("Hello \(danger.github.pullRequest.user.login)")
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
+
+if danger.github.pullRequest.body == nil || danger.github.pullRequest.body!.isEmpty {
+    warn("Please provide a summary in the Pull Request description")
+}
+
+SwiftLint.lint(.modifiedAndCreatedFiles(directory: nil), inline: true, configFile: nil, strict: false, quiet: true, swiftlintPath: nil)
